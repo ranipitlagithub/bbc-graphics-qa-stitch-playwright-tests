cc24a1ccddb05174a0c801f79fef8e44d77992d1
Dave Lee <dave.lee04@bbc.co.uk>
2025-12-16 17:10:53 +0000

    update `auth.setup.js`

--- playwright.config.js
+++ playwright.config.js
@@ -22,33 +22,32 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://stitch.test.tools.bbc.co.uk/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    launchOptions:{
-      slowMo: 1000
-    }
+    launchOptions: {
+      slowMo: 1000,
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-
       name: 'setup',
       testMatch: /auth\.setup\.js/,
     },
     {
       name: 'chromium',
-      use: { 
+      use: {
         ...devices['Desktop Chrome'],
         storageState: path.resolve(__dirname, 'playwright/.auth/user.json'),
-       },
-     
-     dependencies: ['setup'],
+      },
+
+      dependencies: ['setup'],
     },
 
-   /* {
+    /* {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -86,4 +85,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -2,23 +2,19 @@ import { test as setup, expect } from '@playwright/test';
 import { authenticator } from 'otplib';
 import path from 'path';
 
-
-
+const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
-
-  const authFile = path.join(__dirname, '../playwright/.auth/user.json');
-  await page.goto('https://stitch.test.tools.bbc.co.uk/');
+  await page.goto('/');
   await page.getByRole('textbox', { name: 'email' }).fill(process.env.BBC_LOGIN_USER_EMAIL);
-  await page.keyboard.press('Enter');
   await page.getByRole('textbox', { name: 'password' }).fill(process.env.BBC_LOGIN_USER_PASSWORD);
-  await page.keyboard.press('Enter');
- 
-  const token = authenticator.generate(process.env.BBC_LOGIN_OTP_SECRET);
+  await page.getByRole('button', { name: 'Log in' }).click();
 
+  const token = authenticator.generate(process.env.BBC_LOGIN_OTP_SECRET);
   await page.getByLabel('code').fill(token);
   await page.getByRole('button', { name: 'Submit' }).click();
 
+  // Wait for page redirection.
+  await page.waitForURL('/');
   await page.context().storageState({ path: authFile });
-}
-);
+});
